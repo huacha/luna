9570265bc32808a8092e233f111518b7e153c8f1
tomkrus <pzq0331@gmail.com>
2014-09-14 23:29:01 +0800

    update

--- src/main/java/com/luna/bpm/behavior/ProcessEngineBeanFactory.java
+++ src/main/java/com/luna/bpm/behavior/ProcessEngineBeanFactory.java
@@ -4,21 +4,20 @@ import java.util.Map;
 
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.impl.ProcessEngineImpl;
-import org.activiti.engine.impl.cfg.SpringBeanFactoryProxyMap;
 
 public class ProcessEngineBeanFactory {
-    private static Map beanFactory;
+    private static Map<Object,Object> beanFactory;
 
     static {
         if (beanFactory == null) {
             ProcessEngineImpl processEngine = (ProcessEngineImpl) ProcessEngines
                     .getDefaultProcessEngine();
-            beanFactory = (Map) processEngine.getProcessEngineConfiguration()
-                    .getBeans();
+            beanFactory =  processEngine.getProcessEngineConfiguration().getBeans();
         }
     }
 
-    public static <T> T getBean(String beanName) {
+    @SuppressWarnings("unchecked")
+	public static <T> T getBean(String beanName) {
         return (T) beanFactory.get(beanName);
     }
 }
