e2631f567de87c2d686c5ebaff83fa927ee9390c
glenyan <yy.10@163.com>
2014-09-14 22:35:24 +0800

    update CounterSignService

--- src/main/java/com/luna/bpm/persistence/manager/BpmConfCountersignManager.java
+++ src/main/java/com/luna/bpm/persistence/manager/BpmConfCountersignManager.java
@@ -2,10 +2,14 @@ package com.luna.bpm.persistence.manager;
 
 
 
+import org.springframework.data.jpa.repository.Query;
+
+import com.luna.bpm.persistence.domain.BpmConfBase;
 import com.luna.bpm.persistence.domain.BpmConfCountersign;
 import com.luna.common.repository.BaseRepository;
 
 
-public interface BpmConfCountersignManager extends
-        BaseRepository<BpmConfCountersign,Long> {
+public interface BpmConfCountersignManager extends BaseRepository<BpmConfCountersign,Long> {
+	@Query("from BpmConfCountersign where bpmConfNode.bpmConfBase.processDefinitionId=?1 and bpmConfNode.code=?2")
+	BpmConfCountersign findUnique(String processDefinitionId,String activityId);
 }
