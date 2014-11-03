b44695873026ebabaeb58ffea7fe6f3c4a80342e
glenyan <yy.10@163.com>
2014-11-03 16:30:54 +0800

    准备数据库脚本

---
+++ sql/luna-clear-data-Acitviti.sql
@@ -0,0 +1,28 @@
+﻿SET FOREIGN_KEY_CHECKS = 0;
+
+delete from ACT_EVT_LOG            ;
+delete from ACT_GE_BYTEARRAY       ;
+delete from ACT_HI_ACTINST         ;
+delete from ACT_HI_ATTACHMENT      ;
+delete from ACT_HI_COMMENT         ;
+delete from ACT_HI_DETAIL          ;
+delete from ACT_HI_IDENTITYLINK    ;
+delete from ACT_HI_PROCINST        ;
+delete from ACT_HI_TASKINST        ;
+delete from ACT_HI_VARINST         ;
+delete from ACT_ID_GROUP           ;
+delete from ACT_ID_INFO            ;
+delete from ACT_ID_MEMBERSHIP      ;
+delete from ACT_ID_USER            ;
+delete from ACT_RE_DEPLOYMENT      ;
+delete from ACT_RE_MODEL           ;
+delete from ACT_RE_PROCDEF         ;
+delete from ACT_RU_EVENT_SUBSCR    ;
+delete from ACT_RU_EXECUTION       ;
+delete from ACT_RU_IDENTITYLINK    ;
+delete from ACT_RU_JOB             ;
+delete from ACT_RU_TASK            ;
+delete from ACT_RU_VARIABLE        ;
+commit;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ sql/luna-clear-data-BpmProcessConfig.sql
@@ -0,0 +1,24 @@
+﻿SET FOREIGN_KEY_CHECKS = 0;
+
+delete from  BPM_CATEGORY             ;
+delete from  BPM_CONF_BASE            ;
+delete from  BPM_CONF_COUNTERSIGN     ;
+delete from  BPM_CONF_FORM            ;
+delete from  BPM_CONF_LISTENER        ;
+delete from  BPM_CONF_NODE            ;
+delete from  BPM_CONF_NOTICE          ;
+delete from  BPM_CONF_OPERATION       ;
+delete from  BPM_CONF_RULE            ;
+delete from  BPM_CONF_USER            ;
+delete from  BPM_DELEGATE_HISTORY     ;
+delete from  BPM_DELEGATE_INFO        ;
+delete from  BPM_PROCESS              ;
+delete from  BPM_TASK_BUSSINESS       ;
+delete from  BPM_TASK_CONF            ;
+delete from  BPM_TASK_DEF             ;
+delete from  BPM_TASK_DEF_NOTICE      ;
+delete from  FORM_TEMPLATE            ;
+
+commit;
+
+SET FOREIGN_KEY_CHECKS = 1;
